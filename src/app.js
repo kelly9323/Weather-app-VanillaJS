a1835ba03fca4cb41592a684fc0974339a02cfa9
kelly9323 <anastasiashlapak@icloud.com>
2022-10-12 11:19:13 +0200

    added JS

--- src/app.js
+++ src/app.js
@@ -1,42 +1,41 @@
 function formatDate(timestamp) {
-    let date = new Date(timestamp);
-    let hours = date.getHours();
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-    let minutes = date.getMinutes();
-    if (minutes < 10){
-        minutes = `0${minutes}`;
-    }
-    let weekDays = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let monthArray = [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec",
-    ];
-    let month = monthArray[date.getMonth()];
-    let day = date.getDate();
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let weekDays = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let monthArray = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = monthArray[date.getMonth()];
+  let day = date.getDate();
 
-    let weekDay = weekDays[date.getDay()];
-    return `Upd: ${weekDay}, ${day} ${month} </br> ${hours}:${minutes}`;
-
+  let weekDay = weekDays[date.getDay()];
+  return `Upd: ${weekDay}, ${day} ${month} </br> ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
@@ -49,6 +48,10 @@ function displayTemperature(response) {
   let cloudsElement = document.querySelector("#clouds");
   let dateElement = document.querySelector("#weekDay");
   let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+  feelsLikeElement = Math.round(response.data.main.feels_like);
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
@@ -57,20 +60,55 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   cloudsElement.innerHTML = response.data.clouds.all;
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-function search(city){
-let apiKey = "9d7ae4b49b3b06e0a622dcc71f2df3b4";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+  let apiKey = "9d7ae4b49b3b06e0a622dcc71f2df3b4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-function handleSubmit(event){
-    event.preventDefault();
-    let cityInputElement = document.querySelector("#city-input");
-    search(cityInputElement.value);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#city-input");
+  search(cityInputElement.value);
 }
+function showFtemp(event) {
+  event.preventDefault();
+  let temperatureElement = document.querySelector("#temperature");
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  let feelsLike = (feelsLikeElement * 9) / 5 + 32;
+  let tempFeel = document.querySelector("#feelsLike");
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+  tempFeel.innerHTML = Math.round(feelsLike);
+}
+
+function showCtemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temperature");
+  let tempFeel = document.querySelector("#feelsLike");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  tempFeel.innerHTML = Math.round(feelsLikeElement);
+}
+
+let celsiusTemperature = null;
+let feelsLikeElement = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", showFtemp);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", showCtemp);
+
+search("New York");
